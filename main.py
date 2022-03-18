9b4f54519c9be285046c8ee89678f52d0e4ac3f2
Arne Morten Johansen <arjo@equinor.com>
2022-03-18 11:11:43 +0100

    First test

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
 import os
 import requests  # noqa We are just importing this to prove the dependency installed correctly
-
+import json
+from detect_secrets import SecretsCollection
+from detect_secrets.settings import default_settings
+from detect_secrets.core import baseline
+from detect_secrets.core.scan import get_files_to_scan
+#from multiprocessing import freeze_support
 
 def main():
-    my_input = os.environ["INPUT_MYINPUT"]
+    files = json.loads(os.environ["FILES"])
+    baseline_file = os.environ["DS_BASELINE_FILE"]
 
-    my_output = f"Hello {my_input}"
 
-    print(f"::set-output name=myOutput::{my_output}")
+    my_output = f"Hello {files}"
+    print(files)
 
+
+    secrets = SecretsCollection()
+    #secrets.scan_files()
+
+    with default_settings():
+        #secrets.scan_file(r"test.txt")
+        for f in files:
+            print(f)
+            secrets.scan_file('test.txt', 'entrypoint.sh')
+
+
+
+    base = baseline.load(baseline.load_from_file(r".secrets.baseline"))
+
+    new_secrets = secrets - base
+
+    print(json.dumps(base.json(), indent=2))
+    print("-----------------------------------------")
+    print(json.dumps(secrets.json(), indent=2))
+    print("-----------------------------------------")
+    print(json.dumps(new_secrets.json(), indent=2))
 
 if __name__ == "__main__":
     main()
+
+
+
+
